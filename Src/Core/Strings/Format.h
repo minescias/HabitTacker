0b77b1c00cc5bcb7c4eb0f24a87814eeac03c3f6
Dawid Kozłowski <dawid_kozlowski@hotmail.com>
2019-07-18 17:31:36 +0200

    Formating string with multiple parameters
    T: C05

--- Src/Core/Strings/Format.h
+++ Src/Core/Strings/Format.h
@@ -2,24 +2,45 @@
 #define __FORMAT_H
 
 #include <string>
+#include <sstream>
 
-namespace Strings
+namespace Strings::Detail
 {
 
-} // namespace Strings
-
-// template<typename T, typename... TArgs>
-// std::string format()
-
-std::string format(const std::string& str, const std::string& val = "")
+std::string getIdentifier(int index)
 {
-	auto pos = str.find("%1%");
-	auto tmp = str;
-	
-	if (pos != std::string::npos)
-		tmp = tmp.replace(pos, 3, val);	
+	return std::string("%") + std::to_string(index) + "%";
+}
 
-	return tmp;
+template<typename T>
+std::string format(const std::string& str, int index,  T value)
+{
+	auto pos = str.find(getIdentifier(index));
+	std::stringstream ss;
+	ss << str.substr(0, pos) << value << str.substr(pos + 3);
+	return ss.str();
+}
+
+template<typename T, typename... TArgs>
+std::string format(const std::string& str, int index,  T value, TArgs... args)
+{
+	auto pos = str.find(getIdentifier(index));
+	std::stringstream ss;
+	ss << str.substr(0, pos) << value << str.substr(pos + 3);
+	return format(ss.str(), index + 1, args...);
+}
+
+} // namespace Strings::Detail
+
+std::string format(const std::string& str)
+{
+	return str;
+}
+
+template <typename... TArtgs>
+std::string format(const std::string& str, TArtgs... targs)
+{
+	return Strings::Detail::format(str, 1, targs...);
 }
 
 #endif // __FORMAT_H
--- Tests/Core/Strings/FormatTests.cpp
+++ Tests/Core/Strings/FormatTests.cpp
@@ -12,7 +12,7 @@ public:
 	FormatTests(){};
 };
 
-TEST(FormatTest, format_text_without_parameters)
+TEST(FormatTest, formats_text_without_parameters)
 {
 	auto expected = "onetwothree";
 	auto actual = format("onetwothree");
@@ -20,12 +20,32 @@ TEST(FormatTest, format_text_without_parameters)
 	ASSERT_THAT(actual, Eq(expected));
 }
 
-TEST(FormatTest, format_text_with_one_parameter_of_string_type)
+TEST(FormatTest, formats_text_with_one_parameter_of_string_type)
 {
 	auto expected = "firts parameter is: foo";
 	auto actual = format("firts parameter is: %1%", "foo");
 
 	ASSERT_THAT(actual, Eq(expected));
+}
+
+TEST(FormatTest, formats_text_with_one_parameter_of_any_type)
+{
+	auto expected = "firts parameter is: foo";
+	auto actual = format("firts parameter is: %1%", "foo");
+
+	ASSERT_THAT(format("int: %1% OK", 10), Eq("int: 10 OK"));
+	ASSERT_THAT(format("double: %1% OK", 10.0), Eq("double: 10 OK"));
+	ASSERT_THAT(format("double: %1% OK", 10.3), Eq("double: 10.3 OK"));
+	ASSERT_THAT(format("double: %1% OK", 10.3534), Eq("double: 10.3534 OK"));
+}
+
+TEST(FormatTests, formats_text_with_more_than_one_parameter)
+{
+	auto expected = "abc; 123, def 4.56 ghi";
+	auto actual = format("abc; %1%, def %2% ghi", 123, 4.56);
+
+	ASSERT_THAT(actual, Eq(expected));
+	
 }
 
 } // namespace Tests
