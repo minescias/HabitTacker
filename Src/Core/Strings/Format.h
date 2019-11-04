f0b14ab094788835c3bfed270bb50b974594d5b2
Dawid Kozłowski <dawid_kozlowski@hotmail.com>
2019-11-05 00:13:53 +0100

    Log: Marked old  format functions as deprecated

--- Src/Core/Strings/Format.h
+++ Src/Core/Strings/Format.h
@@ -1,32 +1,29 @@
 #ifndef __FORMAT_H
 #define __FORMAT_H
 
-#include <string>
 #include <sstream>
+#include <string>
 
 #include <Core/Exceptions/LogicError.h>
 
 namespace Strings
 {
-
 namespace Detail
 {
-
 inline std::string getIdentifier(int index)
 {
 	return std::string("%") + std::to_string(index) + "%";
 }
 
 template<typename T>
-std::string format(const std::string& str, int index,  T value)
+std::string format(const std::string& str, int index, T value)
 {
 	auto tmpStr = str;
 	auto identifier = getIdentifier(index);
-	
+
 	auto pos = tmpStr.find(identifier);
 	if (pos == std::string::npos)
 		throw LogicError("Strings::format: more variables than placeholders");
-
 
 	while (pos != std::string::npos)
 	{
@@ -41,20 +38,22 @@ std::string format(const std::string& str, int index,  T value)
 }
 
 template<typename T, typename... TArgs>
-std::string format(const std::string& str, int index,  T value, TArgs... args)
+std::string format(const std::string& str, int index, T value, TArgs... args)
 {
-	auto tmpStr = format (str, index, value);
+	auto tmpStr = format(str, index, value);
 	return format(tmpStr, index + 1, args...);
 }
 
 } // namespace Detail
 
+[[deprecated("This function was replaced by fmt::format")]]
 inline std::string format(const std::string& str)
 {
 	return str;
 }
 
-template <typename... TArtgs>
+template<typename... TArtgs>
+[[deprecated("This function was replaced by fmt::format")]]
 std::string format(const std::string& str, TArtgs... targs)
 {
 	return Detail::format(str, 1, targs...);
