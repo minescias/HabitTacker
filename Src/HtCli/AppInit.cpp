c6a4284cbdab726d74d733747a3da8fb13007235
Dawid Kozłowski <dawid_kozlowski@hotmail.com>
2019-11-20 21:20:24 +0100

    HtCli: AppInit refactor

--- Src/HtCli/AppInit.cpp
+++ Src/HtCli/AppInit.cpp
@@ -19,58 +19,51 @@
 
 int appInit(int argc, char* argv[])
 {
-    Cli::CommandLineParser parser;
-    auto parserResult = Cli::CommandLineParser().parse(argc, argv);
-    auto commandName = parserResult.getCommandName();
+	Cli::CommandLineParser parser;
+	auto parserResult = Cli::CommandLineParser().parse(argc, argv);
+	auto commandName = parserResult.getCommandName();
 
-    auto logConfig = std::make_unique<Log::Config>();
-    logConfig->enabled = false;
-    logConfig->levels = {Log::Levels::Error, Log::Levels::Sql};
-    auto logger = std::make_unique<Log::Logger>(std::move(logConfig));
-    Log::setLogger(logger.get());
+	auto logConfig = std::make_unique<Log::Config>();
+	logConfig->enabled = false;
+	logConfig->levels = {Log::Levels::Error, Log::Levels::Sql};
+	auto logger = std::make_unique<Log::Logger>(std::move(logConfig));
+	Log::setLogger(logger.get());
 
-    log ("Execute command: " + commandName);
+	try
+	{
+		if (commandName == "help")
+		{
+			printHelpMessage();
+		}
+		else if (commandName == "version")
+		{
+			printVersionNumber();
+		}
+		else if (commandName == "init")
+		{
+			Actions::InitAction().execute(
+				parserResult.getDefaultParameter(), "htr.ini");
+		}
+		else
+		{
+			auto settings = getSettings("htr.ini");
+			auto database = Db::Database(settings->get("database"));
+			auto daoFactory = AppInit::initDaoFactory(&database);
+			auto actionRegister = registerActions();
+			auto action = actionRegister->get(commandName);
 
-    if (commandName == "help")
-    {
-        printHelpMessage();
-        return 0;
-    }
+			action->setDaoFactory(daoFactory.get());
+			action->execute(parserResult);
+		}
+	}
+	catch (const Actions::ActionError& err)
+	{
+		std::cout << err.what() << "\n";
+	}
+	catch (const RuntimeError& err)
+	{
+		std::cout << err.what() << "\n";
+	}
 
-    if (commandName == "version")
-    {
-        printVersionNumber();
-        return 0;
-    }
-
-    try
-    {
-        if (commandName == "init")
-        {
-            Actions::InitAction().execute(
-                parserResult.getDefaultParameter(), "htr.ini");
-
-            return 0;
-        }
-
-
-        auto settings = getSettings("htr.ini");
-        auto database = Db::Database(settings->get("database"));
-        auto daoFactory = AppInit::initDaoFactory(&database);
-        auto actionRegister = registerActions();
-        auto action = actionRegister->get(commandName);
-
-        action->setDaoFactory(daoFactory.get());
-        action->execute(parserResult);
-    }
-    catch (const Actions::ActionError& err)
-    {
-        std::cout << err.what() << "\n";
-    }
-    catch (const RuntimeError& err)
-    {
-        std::cout << err.what() << "\n";
-    }
-
-    return 0;
+	return 0;
 }
